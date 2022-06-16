41accf8af19124fc0316b50caf05e96ca8c67f6c
+ <+>
2022-06-15 21:15:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const remove = document.getElementById('main');
+    remove.remove()
+const newHeader = document.createElement('h1')
+    newHeader.id = 'victory'
+    newHeader.textContent = 'Alan is the champion'
